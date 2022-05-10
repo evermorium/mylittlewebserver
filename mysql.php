7d0b2403a8368b3d82b8f1417050355c24a89a38
evermorium <limjx20606@kakao.com>
2022-05-11 04:08:59 +0900

    commit

---
+++ mysql.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $user = "root";
+
+    $con = mysqli_connect($servername,$user);
+
+    if($con){
+        die("서버와의 연결 실패!: ".mysqli_connect_error());
+    }
+    echo "서버와의 연결 성공!";
+
+    ?>
