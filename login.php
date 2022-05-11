236edb5dc8e57af0b137411f417e85411d5e10a0
evermorium <limjx20606@kakao.com>
2022-05-12 02:01:39 +0900

    cooom

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 $ID = $_POST['username'];
 $PW = $_POST['password'];
 
-$link = mysqli_connect('localhost','root','password');
+$link = mysql_connect('localhost','root','password');
 if(!$link)
 {
     die('Could not connect: ' . mysql_error());
