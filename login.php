bab95d5759f59ff7ebda4d0bf67b5a4f596295ec
evermorium <limjx20606@kakao.com>
2022-05-12 02:00:44 +0900

    comm

--- login.php
+++ login.php
@@ -3,22 +3,19 @@
 
 $ID = $_POST['username'];
 $PW = $_POST['password'];
-$corID = "temp";
-$corPW = "temp";
 
-
-$conn = mysqli_connect("localhost","root","password");
-if(!$conn)
+$link = mysqli_connect('localhost','root','password');
+if(!$link)
 {
     die('Could not connect: ' . mysql_error());
 }
 echo 'Connected successfully';
-mysql_close($conn);
-
+mysql_close($link);
+/*
 if($ID == $corID && $PW == $corPW)
 {
     require("secu999.php");
 }else require("secu_Web_task_2.html");
-
+*/
 
 ?>
