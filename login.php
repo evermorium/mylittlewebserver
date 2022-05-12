ec7699ba592c9ff05f6409ce180982df2f1f2aaa
evermorium <limjx20606@kakao.com>
2022-05-12 18:43:25 +0900

    ppppooo

--- login.php
+++ login.php
@@ -11,6 +11,19 @@ $db -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 echo "데이터베이스 연결 성공!!<br/>";
 
+$keyword = "%테스트%";
+$no = 1;
+
+$query = "SELECT lid,,profile, uid, upw, joindate from Users ";
+$stmt = $db -> prepare($query);
+$stmt -> execute(array($keyword, $no));
+$result = $stmt->fetchALL(PDO::FETCH_NUM);
+
+for($i = 0; $i < count($result); $i++)
+{
+    printf("%s : %s <br />", $result[$i][0], $result[$i][1]);
+}
+
 } catch(PDOException $e){
     echo $e -> getMessage();
 }
