3b7d1e7fc5d9e2290facadf0ffb3896dffb9dc01
evermorium <limjx20606@kakao.com>
2022-05-12 18:23:56 +0900

    coo

--- login.php
+++ login.php
@@ -1,43 +1,20 @@
 <?php
 
+$dbHost = "localhost";
+$dbName = "Users";
+$dbUser = "root";
+$dbPass = "password";
 
+try {
+$db = new PDO("mysql:host ={$dbHost}; dbname = {$dbName}", $dbUser, $dbPass);
+$db -> setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+echo "데이터베이스 연결 성공!!<br/>";
 
-$conn = mysqli_connect('localhost','root','password');
-if(!$conn)
-{
-    die('could not connect: ' .mysqli_connect_error());
+} catch(PDOException $e){
+    echo $e -> getMessage();
 }
-echo 'Connected';
-
-mysqli_select_db($conn, 'Users');
 
 
-
-/*
-$ID = $_POST['username'];
-$PW = $_POST['password'];
-*/
-
-
-
-
-
-/*$mysqli = new mysqli("localhost","root","password","Users");
-echo $mysqli->host_info . "\n";*/
-/*
-$link = mysql_connect('localhost','root','password');
-if(!$link)
-{
-    die('Could not connect: ' . mysql_error());
-}
-echo 'Connected successfully';
-mysql_close($link);
-*/
-/*
-if($ID == $corID && $PW == $corPW)
-{
-    require("secu999.php");
-}else require("secu_Web_task_2.html");
-*/
 
 ?>
